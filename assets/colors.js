9d0fe3603033594a7d8526e655b2ae555e63349f
Need4Swede <theneed4swede@gmail.com>
2022-03-26 16:45:45 -0700

    Code Styling
    swap colors function

---
+++ assets/colors.js
@@ -0,0 +1,5 @@
+const colorSwap = document.querySelector('.colorful-switch').addEventListener('click', swapColors);
+
+function swapColors(){
+    document.getElementById('css').href='codeblocks-light.css';
+}
