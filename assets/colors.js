b9575113ef4c2302cdbd37f182771ad426e0d737
Need4Swede <theneed4swede@gmail.com>
2022-03-29 10:21:45 -0700

    Code Styling
    removed color swap from console button

--- assets/colors.js
+++ assets/colors.js
@@ -33,26 +33,12 @@ const buttonConsole = document.querySelector('#colorful-switch-cb-2').addEventLi
 function consoleButton(){
     isLight = !isLight;
     if (isLight){
-        document.getElementById('css').href='../assets/codeblocks-light.css';
-        document.body.style.backgroundColor = "#fffcf2";
-        document.body.style.color = "black";
-        let preTag = document.querySelectorAll('pre')
-        preTag.forEach(element => {
-            element.style.border = "1px solid grey";
-        })
-        document.getElementById('color-mode-text').innerText = "Light Mode";
+        document.getElementById('console-mode-text').innerText = "Disable Console";
         // document.getElementById('jsConsole').src="https://jsconsole.com/"
         // document.getElementById('jsConsole').height="200"
     }
     else {
-        document.getElementById('css').href='../assets/codeblocks.css';
-        document.body.style.backgroundColor = "#17191d";
-        document.body.style.color = "white";
-        let preTag = document.querySelectorAll('pre')
-        preTag.forEach(element => {
-            element.style.border = "none";
-        })
-        document.getElementById('color-mode-text').innerText = "Dark Mode";
+        document.getElementById('console-mode-text').innerText = "Enable Console";
         // document.getElementById('jsConsole').height="0"
     }
 }
