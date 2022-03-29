375b026eaa5df4678bf779ba41f1b775998501b5
Need4Swede <theneed4swede@gmail.com>
2022-03-29 10:19:00 -0700

    Code Styling
    added console button

--- assets/colors.js
+++ assets/colors.js
@@ -28,6 +28,35 @@ function swapColors(){
     }
 }
 
+const buttonConsole = document.querySelector('#colorful-switch-cb-2').addEventListener('click', consoleButton);
+
+function consoleButton(){
+    isLight = !isLight;
+    if (isLight){
+        document.getElementById('css').href='../assets/codeblocks-light.css';
+        document.body.style.backgroundColor = "#fffcf2";
+        document.body.style.color = "black";
+        let preTag = document.querySelectorAll('pre')
+        preTag.forEach(element => {
+            element.style.border = "1px solid grey";
+        })
+        document.getElementById('color-mode-text').innerText = "Light Mode";
+        // document.getElementById('jsConsole').src="https://jsconsole.com/"
+        // document.getElementById('jsConsole').height="200"
+    }
+    else {
+        document.getElementById('css').href='../assets/codeblocks.css';
+        document.body.style.backgroundColor = "#17191d";
+        document.body.style.color = "white";
+        let preTag = document.querySelectorAll('pre')
+        preTag.forEach(element => {
+            element.style.border = "none";
+        })
+        document.getElementById('color-mode-text').innerText = "Dark Mode";
+        // document.getElementById('jsConsole').height="0"
+    }
+}
+
 window.onload = function() {
     let myiFrame = document.getElementById("jsConsole");
     let doc = myiFrame.contentDocument;
