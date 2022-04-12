d9a992dff4230453e0aea12572b775b04d1aa6fe
Need4Swede <theneed4swede@gmail.com>
2022-04-11 20:22:14 -0700

    Codewars Solutions
    added getMiddle()

--- codewars.js
+++ codewars.js
@@ -142,3 +142,9 @@ function findCapitals(word){
   // Output the array
   return indexArr;
 }
+function getMiddle(s){
+  if (s.length % 2 === 0){
+    return s = (s[s.length / 2 - 1]) + (s[s.length / 2]);
+  }
+  return s[(s.length - 1) / 2]
+}
